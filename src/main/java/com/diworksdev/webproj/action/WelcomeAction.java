8d58f8cbabd9bdb89a79e9f1e050e885aee9bc8a
kazu0112 <kazuma0112@icloud.com>
2026-06-01 17:20:47 +0900

    WelcomeAction.java修正、welcome.jsp作成

--- src/main/java/com/diworksdev/webproj/action/WelcomeAction.java
+++ src/main/java/com/diworksdev/webproj/action/WelcomeAction.java
@@ -2,12 +2,9 @@ package com.diworksdev.webproj.action;
 
 import com.opensymphony.xwork2.ActionSupport;
 
-public class WelcomeAction {
-	
-	public class WelcomeAction extends ActionSupport{
-		public String execute()
+public class WelcomeAction extends ActionSupport{
+	public String execute()
 		{ return SUCCESS;
-		
 		}
-	}
+	
 }
